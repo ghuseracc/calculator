e531aa12359622a366f5aab84a25fd65eb79d10a
username <nkuluthuli@gmail.com>
2022-12-13 15:31:56 +0200

    Add operate function to evaluate two numbers

--- javascript.js
+++ javascript.js
@@ -14,3 +14,19 @@ function divide(a,b) {
     return parseInt(a / b);
 };
 
+function operate(operator,a,b) { 
+    switch (operator) {
+        case '+':
+            return add(a,b);
+            break;
+        case '-':
+            return subtract(a,b);
+            break;
+        case '*':
+            return multiply(a,b);
+            break;
+        case '/':
+            return divide(a,b);
+    }
+};
+
